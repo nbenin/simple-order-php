ccf655f2eb9ed9793b083d6df132bc824d575172
Neil <neil.mf.benin@gmail.com>
2020-02-11 14:27:11 +0100

    making it better i guess?

--- form-view.php
+++ form-view.php
@@ -25,7 +25,7 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" class="form-control" value="<?php echo $_SESSION['userEmail'] ?>"/>
+                <input type="text" id="email" name="email" class="form-control" value="<?php echo $userEmail ?>"/>
             </div>
             <div></div>
         </div>
@@ -36,21 +36,21 @@
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $_SESSION['userStreet'] ?>"/>
+                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $userStreet ?>"/>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $_SESSION['userStreetNumber'] ?>"/>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $userStreetNumber ?>"/>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $_SESSION['userCity'] ?>"/>
+                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $userCity ?>"/>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $_SESSION['userZip'] ?>"/>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $userZip ?>"/>
                 </div>
             </div>
         </fieldset>
--- index.php
+++ index.php
@@ -4,18 +4,33 @@ declare(strict_types=1);
 
 //we are going to use session variables so we need to enable sessions
 session_start();
+whatIsHappening();
 
+// Handle closing of the tab or changing to other food tabs
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if ($_SESSION['email'] != '' || $_SESSION['street'] != '' || $_SESSION['streetnumber'] != '' || $_SESSION['city'] != '' || $_SESSION['zipcode'] != '') {
+        $userEmail = $_SESSION['email'];
+        $userStreet = $_SESSION['street'];
+        $userStreetNumber = $_SESSION['streetnumber'];
+        $userCity = $_SESSION['city'];
+        $userZip = $_SESSION['zipcode'];
+    }
+}
+
+// Handle submitting of form
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    // Set variables we will be checking and counter for success
     $userEmail = $userStreet = $userStreetNumber = $userCity = $userZip = '';
+    $_SESSION['email'] = $_SESSION['street'] = $_SESSION['streetnumber'] = $_SESSION['city'] = $_SESSION['zipcode'] = '';
+
+    // Set variables we will be checking and counter for success
     $successCounter = 0;
 
     // Check if valid email format, otherwise send alert
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) || empty($_POST['email'])) {
         echo "<div class='alert alert-warning'><strong>Oops! </strong>Please enter valid email!</div>";
     } else {
-        $userEmail = $_SESSION['userEmail'] = test_input($_POST['email']);
+        $userEmail = $_SESSION['email'] = test_input($_POST['email']);
         $successCounter++;
     }
 
@@ -23,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (preg_match("/[a-zA-Z0-9 ]\\./", $_POST['street']) || empty($_POST['street'])) {
         echo "<div class='alert alert-warning'><strong>Oops! </strong>Please enter valid street name! (letters, spaces, numbers, periods)!</div>";
     } else {
-        $userStreet = $_SESSION['userStreet'] = test_input($_POST['street']);
+        $userStreet = $_SESSION['street'] = test_input($_POST['street']);
         $successCounter++;
     }
 
@@ -31,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (!is_numeric($_POST['streetnumber']) || empty($_POST['streetnumber'])) {
         echo "<div class='alert alert-warning'><strong>Oops! </strong>Please enter valid street number! (Only real numbers)</div>";
     } else {
-        $userStreetNumber = $_SESSION['userStreetNumber'] = test_input($_POST['streetnumber']);
+        $userStreetNumber = $_SESSION['streetnumber'] = test_input($_POST['streetnumber']);
         $successCounter++;
     }
 
@@ -39,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (!ctype_alpha($_POST['city']) || empty($_POST['city'])) {
         echo "<div class='alert alert-warning'><strong>Oops! </strong>Please enter valid city name! (letters, spaces, numbers, periods)!</div>";
     } else {
-        $userCity = $_SESSION['userCity'] = test_input($_POST['city']);
+        $userCity = $_SESSION['city'] = test_input($_POST['city']);
         $successCounter++;
     }
 
@@ -47,14 +62,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (!is_numeric($_POST['zipcode']) || empty($_POST['zipcode'])) {
         echo "<div class='alert alert-warning'><strong>Oops! </strong>Please enter valid zip code! (Only real numbers)</div>";
     } else {
-        $userZip = $_SESSION['userZip'] = test_input($_POST['zipcode']);
+        $userZip = $_SESSION['zipcode'] = test_input($_POST['zipcode']);
         $successCounter++;
     }
 
     // Else success
     if ($successCounter == 5) {
         echo "<div class='alert alert-success'><strong>Good Job! </strong> Form submitted succesfully!</div>";
-        unset($_SESSION['userEmail']);
+        $userEmail = $_SESSION['email'] = '';
         $successCounter = 0;
     }
 }
@@ -67,8 +82,6 @@ function test_input($data) {
     return $data;
 }
 
-
-whatIsHappening();
 function whatIsHappening() {
     echo '<h2>$_SERVER</h2>';
     var_dump($_SERVER);
@@ -85,7 +98,7 @@ function whatIsHappening() {
 // Set food depending on foodGET
 $food = $_GET['food'];
 
-//your products with their price.
+// Our products with their price.
 if ($food == null || $food == 1) {
     $products = [
         ['name' => 'Club Ham', 'price' => 3.20],
